d732ad6888ad6f8dc0f5d3e0043871ff12b8756d
avetisminasyan <avominasyan71@gmail.com>
2023-02-16 19:24:06 +0400

    TREE-5

--- tree.sh
+++ tree.sh
@@ -5,13 +5,20 @@ do
        	if [ -d $content ]
 	then	
 		dir_c=$(( $dir_c +1 ))
-		#if [ ! `ls -A $content/ | wc -m` == "0" ];
-		#then	
-		d="$(basename -- $content)"
-		echo -e "|$a$d"
-		a+="-"
-   		func_m $content
-		a="${a::-1}"
+		if [ ! `ls -A $content/ | wc -m` == "0" ];
+		then	
+			d="$(basename -- $content)"
+			echo -e "|$a$d"
+			a+="-"
+   			func_m $content
+			a="${a::-1}"
+		else
+			a+="-"
+			d="$(basename -- $content)"
+			echo -e "|$a$d"
+			a="${a::-1}"
+		fi
+
 	else	
 		file_c=$(( $file_c +1  ))
 		f="$(basename -- $content)"
