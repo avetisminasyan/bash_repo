ddd7d61c2f612c096f14352d8facdedb7255787f
avetisminasyan <avominasyan71@gmail.com>
2023-02-16 18:47:42 +0400

    Tree changing

--- tree.sh
+++ tree.sh
@@ -7,21 +7,21 @@ do
 		if [ ! `ls -A $content/ | wc -m` == "0" ];
 		then	
 			d="$(basename -- $content)"
-			echo -e "$a $d --THis is directory"
-			a+=" "
+			echo -e "$|$a$d --THis is directory"
+			a+="-"
    			func_m $content
 #			echo "THis is not empy dir $content  "
 			a="${a::-1}"
 		fi
 	else	file_c=$(( $file_c +1  ))
 		f="$(basename -- $content)"
-		echo -e  "$a $f"
+		echo -e  "|$a$f"
 	fi
 done
 }
 #func_m $PWD
 main(){
-a=""
+a="-"
 dir_c=0
 file_c=0
 func_m $PWD
